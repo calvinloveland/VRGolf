d478e36b5d4631bb8d84dc689d9ee3efd77da87c
ChrisBrown46 <christopher.r.brown@aggiemail.usu.edu>
2019-03-30 01:17:35 -0600

    Added speed boost and anti-gravity triggers

---
+++ Assets/Scripts/AntiGravityTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AntiGravityTrigger : MonoBehaviour {
+
+    public float hoverForce = 12f;
+
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("Object entered the trigger");
+    }
+
+    void OnTriggerStay(Collider other) {
+        other.GetComponent<Rigidbody>().AddForce(Vector3.up * hoverForce, ForceMode.Acceleration);
+        Debug.Log("Object is within trigger");
+    }
+
+    void OnTriggerExit(Collider other) {
+        Debug.Log("Object exited the trigger");
+    }
+}
---
+++ Assets/Scripts/SpeedBoostTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SpeedBoostTrigger : MonoBehaviour {
+
+    public float speedBoostForce = 12f;
+
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("Object entered the trigger");
+    }
+
+    void OnTriggerStay(Collider other) {
+        other.GetComponent<Rigidbody>().AddForce(Vector3.forward * speedBoostForce, ForceMode.Acceleration);
+        Debug.Log("Object is within trigger");
+    }
+
+    void OnTriggerExit(Collider other) {
+        Debug.Log("Object exited the trigger");
+    }
+}
