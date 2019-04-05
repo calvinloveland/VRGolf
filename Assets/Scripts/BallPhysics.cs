8d418737bfb4d0a3b75d85d9900cb7cfa9d5e38e
Calvin Loveland <calvinloveland@gmail.com>
2019-04-05 17:13:08 -0600

    All those things we missed

---
+++ Assets/Scripts/BallPhysics.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class BallPhysics : MonoBehaviour {
+
+    public GameObject playerCamera;
+    public float stoppingThreshold;
+    public float timeUntilStopping;
+
+    private float timer = 0.0f;
+    private Vector3 offset;
+
+    void Start() {
+        stoppingThreshold = GetComponent<BallPhysics>().stoppingThreshold;
+        offset = playerCamera.transform.position - transform.position;
+    }
+
+    void Update() {
+        timer += Time.deltaTime;
+        if (timer > timeUntilStopping) {
+            if (GetComponent<Rigidbody>().velocity.magnitude <= stoppingThreshold) {
+                GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+                GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+                playerCamera.transform.position = transform.position + offset;
+                timer = 0.0f;
+            }
+        }
+    }
+}
