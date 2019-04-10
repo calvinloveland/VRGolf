d82946f37f55bb727764d2e19593a03b91a3e9bd
Christopher Brown <christopher.r.brown@aggiemail.usu.edu>
2019-04-09 20:13:37 -0600

    Updated work on VR golf club handling

---
+++ Assets/Scripts/GolfClub.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GolfClub : MonoBehaviour {
+
+    public GameObject golfBall;
+    public float sensitivity = 100f;
+
+    public void OnTriggerEnter(Collider other) {
+        if (other.gameObject != golfBall) return;
+
+        ;
+        other.GetComponent<Rigidbody>().AddForce((Vector3.one + other.GetComponent<Rigidbody>().velocity.normalized) * sensitivity, ForceMode.Acceleration);
+    }
+}
